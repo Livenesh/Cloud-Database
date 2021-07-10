e8194c44bea36eb3b9bf3a04028e94a8cea32388
Livenesh <81608508+Livenesh@users.noreply.github.com>
2021-07-11 04:20:41 +0800

    Update index.js

--- index.js
+++ index.js
@@ -35,14 +35,3 @@ mongoose.connect( 'mongodb+srv://m001-student:m001-mongodb-basics@sandbox.7sjg7.
 app.listen(PORT, () => 
       console.log('Listening on Port')
 );
-
-//const express = require('express')
-//const path = require('path')
-//const PORT = process.env.PORT || 5000
-
-// express()
-//  .use(express.static(path.join(__dirname, 'public')))
-//  .set('views', path.join(__dirname, 'views'))
-//  .set('view engine', 'ejs')
-//  .get('/', (req, res) => res.render('pages/index'))
-//  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
